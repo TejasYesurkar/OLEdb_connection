37fe6cb84b0d84e48e0b1b465063a5b243104a83
TejasYesurkar <47203974+TejasYesurkar@users.noreply.github.com>
2019-07-21 15:02:34 +0530

    Add files via upload

---
+++ Registration/Registration/Form1.Designer.vb
@@ -0,0 +1,338 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRegistration
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtid = New System.Windows.Forms.TextBox()
+        Me.txtmob = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtactive = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtreq = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.date_inst = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtcust = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.txtap = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.txtsr = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtemail = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtnoclient = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtsub = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.mobapp = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.int_date = New System.Windows.Forms.TextBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Location = New System.Drawing.Point(254, 352)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(172, 67)
+        Me.btnSubmit.TabIndex = 13
+        Me.btnSubmit.Text = "Register"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(100, 32)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ID"
+        '
+        'txtid
+        '
+        Me.txtid.Location = New System.Drawing.Point(131, 32)
+        Me.txtid.Name = "txtid"
+        Me.txtid.Size = New System.Drawing.Size(203, 20)
+        Me.txtid.TabIndex = 0
+        '
+        'txtmob
+        '
+        Me.txtmob.Location = New System.Drawing.Point(131, 276)
+        Me.txtmob.Name = "txtmob"
+        Me.txtmob.Size = New System.Drawing.Size(203, 20)
+        Me.txtmob.TabIndex = 7
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(47, 283)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(78, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Mobile Number"
+        '
+        'txtactive
+        '
+        Me.txtactive.Location = New System.Drawing.Point(131, 235)
+        Me.txtactive.Name = "txtactive"
+        Me.txtactive.Size = New System.Drawing.Size(203, 20)
+        Me.txtactive.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(45, 242)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(80, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Activation Data"
+        '
+        'txtreq
+        '
+        Me.txtreq.Location = New System.Drawing.Point(131, 196)
+        Me.txtreq.Name = "txtreq"
+        Me.txtreq.Size = New System.Drawing.Size(203, 20)
+        Me.txtreq.TabIndex = 5
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(50, 203)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(75, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Request Code"
+        '
+        'date_inst
+        '
+        Me.date_inst.Location = New System.Drawing.Point(131, 165)
+        Me.date_inst.Name = "date_inst"
+        Me.date_inst.Size = New System.Drawing.Size(203, 20)
+        Me.date_inst.TabIndex = 4
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(28, 171)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(97, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Date Of Installation"
+        '
+        'txtcust
+        '
+        Me.txtcust.Location = New System.Drawing.Point(131, 130)
+        Me.txtcust.Name = "txtcust"
+        Me.txtcust.Size = New System.Drawing.Size(203, 20)
+        Me.txtcust.TabIndex = 3
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(43, 137)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(82, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Customer Name"
+        '
+        'txtap
+        '
+        Me.txtap.Location = New System.Drawing.Point(131, 96)
+        Me.txtap.Name = "txtap"
+        Me.txtap.Size = New System.Drawing.Size(203, 20)
+        Me.txtap.TabIndex = 2
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(61, 103)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(57, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "App Name"
+        '
+        'txtsr
+        '
+        Me.txtsr.Location = New System.Drawing.Point(131, 61)
+        Me.txtsr.Name = "txtsr"
+        Me.txtsr.Size = New System.Drawing.Size(203, 20)
+        Me.txtsr.TabIndex = 1
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(65, 64)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(53, 13)
+        Me.Label8.TabIndex = 15
+        Me.Label8.Text = "Serial No."
+        '
+        'txtemail
+        '
+        Me.txtemail.Location = New System.Drawing.Point(459, 35)
+        Me.txtemail.Name = "txtemail"
+        Me.txtemail.Size = New System.Drawing.Size(203, 20)
+        Me.txtemail.TabIndex = 8
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(395, 39)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(46, 13)
+        Me.Label9.TabIndex = 17
+        Me.Label9.Text = "Email ID"
+        '
+        'txtnoclient
+        '
+        Me.txtnoclient.Location = New System.Drawing.Point(459, 64)
+        Me.txtnoclient.Name = "txtnoclient"
+        Me.txtnoclient.Size = New System.Drawing.Size(203, 20)
+        Me.txtnoclient.TabIndex = 9
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(354, 67)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(87, 13)
+        Me.Label10.TabIndex = 19
+        Me.Label10.Text = "Number Of Client"
+        '
+        'txtsub
+        '
+        Me.txtsub.Location = New System.Drawing.Point(459, 96)
+        Me.txtsub.Name = "txtsub"
+        Me.txtsub.Size = New System.Drawing.Size(203, 20)
+        Me.txtsub.TabIndex = 10
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(340, 99)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(109, 13)
+        Me.Label11.TabIndex = 21
+        Me.Label11.Text = "Subscrition vdty. date" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'mobapp
+        '
+        Me.mobapp.Location = New System.Drawing.Point(459, 129)
+        Me.mobapp.Name = "mobapp"
+        Me.mobapp.Size = New System.Drawing.Size(203, 20)
+        Me.mobapp.TabIndex = 11
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(340, 132)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(113, 13)
+        Me.Label12.TabIndex = 23
+        Me.Label12.Text = "Mobile App Integration"
+        '
+        'int_date
+        '
+        Me.int_date.Location = New System.Drawing.Point(459, 164)
+        Me.int_date.Name = "int_date"
+        Me.int_date.Size = New System.Drawing.Size(203, 20)
+        Me.int_date.TabIndex = 12
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(363, 171)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(86, 13)
+        Me.Label13.TabIndex = 25
+        Me.Label13.Text = "Intergration Date"
+        '
+        'frmRegistration
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(793, 480)
+        Me.Controls.Add(Me.int_date)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.mobapp)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.txtsub)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.txtnoclient)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.txtemail)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.txtsr)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtap)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.txtcust)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.date_inst)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtreq)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtactive)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtmob)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtid)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.KeyPreview = True
+        Me.Name = "frmRegistration"
+        Me.Text = "Registration Form"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtid As TextBox
+    Friend WithEvents txtmob As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtactive As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtreq As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents date_inst As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtcust As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents txtap As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents txtsr As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtemail As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents txtnoclient As TextBox
+    Friend WithEvents Label10 As Label
+    Friend WithEvents txtsub As TextBox
+    Friend WithEvents Label11 As Label
+    Friend WithEvents mobapp As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents int_date As TextBox
+    Friend WithEvents Label13 As Label
+End Class
---
+++ Registration/Registration/Form1.vb
@@ -0,0 +1,51 @@
+﻿Imports System.Data.OleDb
+Imports System
+Imports System.Data
+Imports System.Data.SqlClient
+
+
+Public Class frmRegistration
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        Dim id As String = txtid.Text
+        Dim srno As Integer = Val(txtsr.Text)
+        Dim app As String = txtap.Text
+        Dim cust_nm As String = txtcust.Text
+        Dim date_instaal As Date = CDate(date_inst.Text)
+        Dim req_code As String = txtreq.Text
+        Dim active_data As String = txtactive.Text
+        Dim mobile As Integer = Val(txtmob.Text)
+        Dim email As String = txtemail.Text
+        Dim no_client As Integer = Val(txtnoclient.Text)
+        Dim sub_date As Date = txtsub.Text
+        Dim mob_app As Integer = CBool(mobapp.Text)
+        Dim int_date As Date = Me.int_date.Text
+        Dim sql As String
+
+
+
+        Dim ds As New DataSet
+        ds = New DataSet("users")
+        Dim OleConn As OleDb.OleDbConnection
+        OleConn = New OleDb.OleDbConnection
+        OleConn.ConnectionString = "Provider=PROVIDER; Data Source=PUT_DATA_SRC;Initial Catalog=;User Id=MY_USERNAME;Password=MY_PSWDS;"
+			' Put your ConnectionString Here
+
+	   MsgBox("Connection open")
+       
+        sql = "INSERT INTO Registration (AutoID,SrNo,AppName,CustName,DoI,ReqCode,ActivationKey,MobileNo,email,Client,ValidTill, MobileAppInt,IntegrationDate)
+                                        VALUES(" & id & "," & srno & ",'" & app & "','" & cust_nm & "','" & date_instaal & "','" & req_code & "','" & active_data & "'," & mobile & ",'" & email & "'," & no_client & ",'" & sub_date & "'," & mob_app & ",'" & int_date & "')"
+			 'Put Insert Command here
+        MsgBox(sql)
+        Dim dataAdptr As OleDb.OleDbDataAdapter = New OleDb.OleDbDataAdapter
+        Try
+            OleConn.Open()
+            dataAdptr.InsertCommand = New OleDbCommand(sql, OleConn)
+            dataAdptr.InsertCommand.ExecuteNonQuery()
+            MsgBox("Row(s) Inserted !! ")
+        Catch ex As Exception
+            MsgBox(ex.ToString)
+        End Try
+        '
+        OleConn.Close()
+     End Sub
+End Class
